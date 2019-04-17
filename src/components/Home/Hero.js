afbe7ab156892bd7aa8f101efea8a5d8244a6e5a
Christopher Servius <spidergon@gmail.com>
2019-04-17 10:42:52 -0300

    home hero background image

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,14 +1,57 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { StaticQuery, graphql } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
 
-const Wrapper = styled.section`
-  padding-top: 5rem;
-  height: 100vh;
-  background-color: lightgray;
+const BackgroundSection = ({ className }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "forest.jpg" }) {
+          childImageSharp {
+            fluid(quality: 100, maxWidth: 4160) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <BackgroundImage
+        className={className}
+        fluid={data.desktop.childImageSharp.fluid}
+        Tag='section'
+      >
+        <div className='hero__inner'>
+          <div className='hero__content'>{'HERO'}</div>
+        </div>
+      </BackgroundImage>
+    )}
+  />
+)
+
+const StyledBackgroundSection = styled(BackgroundSection)`
+  height: 475px;
+  background-position: top center;
+  background-repeat: repeat-y;
+  background-size: cover;
+  margin-bottom: 25px;
+  .hero__inner {
+    padding: 150px 0;
+    color: #fff;
+    height: 100%;
+    .hero__content {
+      padding-left: 55px;
+      padding-right: 55px;
+      max-width: 1200px;
+      margin: 0 auto;
+    }
+  }
 `
 
-function Hero () {
-  return <Wrapper>{'Hero'}</Wrapper>
+BackgroundSection.propTypes = {
+  className: PropTypes.string.isRequired
 }
 
-export default Hero
+export default () => <StyledBackgroundSection />
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,12 +12,12 @@ const Home = () => (
     <Hero />
     <section className='lorem'>
       <div className='inner wrap'>
-        <h1>{'Hi people'}</h1>
+        {/* <h1>{'Hi people'}</h1>
         <p>{'Welcome to your new Gatsby site.'}</p>
         <p>{'Now go build something great.'}</p>
         <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
           <Image />
-        </div>
+        </div> */}
         <Link to={dashPath}>{'Dashboard'}</Link>
         {' | '}
         <Link to={'/store/ma-vitrine-2'}>{'Ma Vitrine 2'}</Link>
