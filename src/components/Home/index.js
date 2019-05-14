60cc2829eef40e4b9cc410644e4d8ae5f20b1309
Christopher Servius <spidergon@gmail.com>
2019-05-13 22:58:11 -0300

    update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -13,8 +13,8 @@ const Home = () => (
     <section className='lorem'>
       <div className='inner wrap'>
         {/* <h1>{'Hi people'}</h1>
-        <p>{'Welcome to your new Gatsby site.'}</p>
-        <p>{'Now go build something great.'}</p>
+        <p>{`{'Welcome to your new Gatsby site.'}̀`}</p>
+        <p>{`{'Now go build something great.'}̀`}</p>
         <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
           <Image />
         </div> */}
@@ -23,58 +23,129 @@ const Home = () => (
         <Link to={dashPath}>{'Dashboard'}</Link>
         {' | '}
         <Link to={'/store/le-bon-resto'}>{'Le Bon Resto'}</Link>
-        <p>
-          {`Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel soluta itaque dolor magnam dicta blanditiis, ducimus obcaecati modi dolores non sint sit sunt vitae quia? Ad dolor a qui doloremque?
-    Eum officia fugiat corrupti soluta amet aperiam veritatis distinctio sapiente dolorum suscipit debitis architecto nobis consequuntur commodi qui deleniti, dolor blanditiis impedit fuga sequi, tempore inventore velit. Id, voluptatibus ipsam.
-    Quasi, obcaecati incidunt minima praesentium aperiam possimus quia alias amet id commodi atque aliquam excepturi molestiae accusamus voluptatibus expedita tenetur officiis assumenda totam facilis eveniet ab quos provident? Autem, temporibus.
-    Fugit quod nam atque dolorum ex, facere maiores vel voluptates dolores minus, incidunt inventore provident dignissimos. Dolorum architecto ratione natus odit fugit suscipit fugiat, nostrum, unde error tempora dignissimos in.
-    Ad aliquid exercitationem dolores dolore temporibus hic quia facilis omnis perferendis illum molestias veritatis vel totam eum, autem tempora dolor animi. Dolorum, harum distinctio. Maiores excepturi suscipit est ipsa ea?
-    Consequatur vero, nostrum iure minima dolore obcaecati praesentium at perferendis, repellendus beatae voluptas error. Sunt aliquid, dolores dolorum alias numquam consectetur, iure incidunt explicabo modi error totam quibusdam esse eum!
-    Quaerat, deserunt atque eos maxime neque modi placeat sit quisquam repellendus expedita reiciendis aperiam! Temporibus, in saepe. Pariatur, doloribus, nisi quia numquam aliquam eum consequuntur maiores quod doloremque, molestiae obcaecati?
-    Magnam fugiat voluptatibus enim culpa dolores soluta ratione, ipsa cupiditate nisi animi tenetur similique voluptates? Obcaecati, ducimus ab a asperiores itaque veniam minus harum omnis tempora, optio quia nam exercitationem!
-    Asperiores rem deserunt iusto beatae. Excepturi distinctio fugit cumque. Ratione exercitationem dolor, perspiciatis eum ab libero in eligendi accusamus voluptatem perferendis rem, soluta est. Consequatur, porro. Omnis suscipit adipisci dolorem.
-    Eius sapiente quod fugiat minima, eos distinctio alias consectetur ratione ab quae. Nostrum blanditiis perspiciatis eius odit perferendis qui, soluta odio impedit vitae optio porro sed quam beatae dolore modi.
-    Eum corporis perspiciatis ullam maiores ipsam, ad dolorum, iste aut deleniti fuga odio ut qui omnis itaque molestiae non! Atque suscipit temporibus dolorem voluptatum nostrum culpa neque ex quasi dolore?
-    Rem magni mollitia velit sequi in atque deserunt aliquid autem fugit nam reprehenderit pariatur, animi dolore ad dicta amet neque placeat. Quasi dolorem, facere rerum facilis eveniet corporis pariatur deleniti!
-    Labore iusto, fugit deleniti iure ea reprehenderit temporibus illo praesentium explicabo. Nobis culpa, cumque obcaecati, vitae repudiandae voluptate veniam, sit eum nemo corrupti asperiores temporibus. Accusamus vitae rerum quis inventore!
-    Accusamus quod, voluptatum, pariatur porro, asperiores harum facere architecto nostrum tempore adipisci doloribus? Cupiditate, provident atque! Possimus consectetur exercitationem accusantium non, ullam illo animi maxime natus incidunt nam, veritatis adipisci?
-    Doloribus, doloremque. Distinctio rerum minus pariatur molestiae reiciendis! Saepe, ipsa doloribus quia itaque nam aut recusandae id nemo porro praesentium tenetur, at non sed autem libero commodi. Eos, maxime eligendi.
-    Odio reiciendis, commodi id saepe suscipit, sint dignissimos officia nobis sed numquam eius quod libero, recusandae officiis autem. Nostrum, itaque aut architecto eligendi recusandae expedita accusantium eaque placeat sit nulla!
-    Eligendi, sit aliquam quas magni distinctio dolore repellendus corporis libero, obcaecati cumque, iusto ipsa assumenda! Odio, sapiente porro voluptatibus consectetur ab unde voluptas aperiam sunt rerum id dicta architecto aut!
-    Consectetur atque ab modi, ex repudiandae vero ducimus suscipit omnis saepe eveniet. Officiis iure fugiat deleniti in perferendis rerum iusto! Quam tempore ullam reprehenderit omnis labore veniam voluptates repudiandae excepturi?
-    Autem, cupiditate in? Nam iste sapiente maxime magnam, debitis exercitationem aut perspiciatis quidem quae veniam nostrum beatae doloribus repudiandae reprehenderit temporibus tempora consectetur officia quam libero et ipsam assumenda! Molestias.
-    Alias dicta commodi ipsa tenetur. Odio doloribus nulla aliquam, velit eius quos et ab, qui consequatur vero excepturi aliquid veritatis, cupiditate earum minima. Ducimus debitis libero assumenda a quidem exercitationem.
-    Nisi, vel nostrum ad nihil non voluptatem et reiciendis voluptates architecto velit tempora obcaecati. Iusto non eius aliquid voluptatibus! Eum magni modi ratione ipsam eius qui expedita laudantium delectus rerum.
-    Delectus recusandae sit amet ea quae dolorum temporibus numquam, autem praesentium debitis assumenda totam quaerat officiis dicta suscipit neque dolore non impedit ratione cupiditate, mollitia nihil repellendus fugiat iste. Itaque?
-    Dolor, dicta veniam sequi quidem iste qui ut! Et esse ab natus omnis libero animi nulla fugiat quasi commodi, voluptatum sit repellat dolorem ipsa aliquid quis dolores doloribus, laborum iusto.
-    Dolorem harum atque hic sed, rerum exercitationem. Fugiat, culpa vero nisi optio assumenda praesentium ea eveniet accusamus nostrum a laudantium tenetur! Cupiditate commodi quidem dolorum alias culpa impedit fugiat nam.
-    Aut, repudiandae cumque. Amet sit distinctio ipsum recusandae. Ipsa, iusto similique architecto, rem accusamus quo quidem, natus praesentium atque recusandae illum dolor omnis voluptatum. Quod tempora consequuntur placeat optio odit?
-    Modi ducimus exercitationem commodi at repellat aperiam debitis fugiat deserunt accusantium amet molestiae iste ex saepe voluptates expedita odit, sint neque error totam possimus corporis, id iure! Fuga, tempora accusantium.
-    Quidem aliquam qui error pariatur eligendi itaque quo veniam, illum, quis delectus possimus omnis libero commodi atque fugiat adipisci dolore corporis cumque, maxime eius vel! Sequi facere officia culpa sit!
-    Suscipit eligendi possimus officia error iste quo, esse dicta dignissimos dolorem maiores vel. Fugiat, iure. Sint suscipit quos laborum, magni culpa, dolore eaque neque facilis qui beatae distinctio voluptatum reiciendis?
-    Molestias aperiam dicta nihil laboriosam optio nostrum saepe et, delectus adipisci molestiae inventore blanditiis dignissimos beatae omnis fuga ipsum pariatur fugiat voluptatem deleniti rerum nobis! Distinctio cumque illo laudantium voluptatibus.
-    Officiis velit sapiente iure delectus itaque sit animi repellat quisquam fugiat sequi esse, dolores inventore maiores voluptas nobis? Voluptates, fugiat fuga. Neque, sint dolorum! Commodi quis accusamus voluptate dolore esse?
-    Dignissimos accusantium, asperiores voluptas ducimus id obcaecati unde in? Deserunt vitae amet minima obcaecati nostrum suscipit dignissimos porro expedita sed ea est tempore voluptatibus, repellat odit magni cum dolor? Dolorem!
-    Explicabo unde aspernatur possimus exercitationem ad iste autem soluta excepturi? Aliquid incidunt possimus eum ut autem sequi, similique doloremque magnam accusamus dolor. Hic blanditiis amet laboriosam reprehenderit qui ad odit?
-    Alias cupiditate quod, quidem quae illo provident corrupti a non dicta? Veritatis quisquam consequuntur delectus vel illo corrupti accusantium non error! Exercitationem ex facere deleniti, recusandae et aliquam sunt cum.
-    Rem quaerat laborum ea dignissimos quidem esse culpa numquam voluptatem aliquam distinctio? Culpa delectus perferendis sint alias, provident dignissimos unde ducimus? Molestiae dolore dignissimos tenetur tempora enim deserunt, obcaecati modi?
-    Hic necessitatibus reiciendis perferendis quis soluta eligendi exercitationem sint cum unde qui veniam placeat, voluptas harum deserunt? Unde dolorem culpa repudiandae adipisci debitis eius eum, neque tempora amet facere quis.
-    Temporibus exercitationem, distinctio dolorum ipsam nisi porro ea at consequuntur corporis labore reiciendis. Dolore maxime suscipit fugit repellat quaerat excepturi, minus aspernatur. Rem accusamus sint autem numquam? Provident, debitis mollitia?
-    Quae excepturi voluptatem est ea reiciendis tenetur amet labore eligendi, ipsam quidem sit fuga quibusdam? Sapiente officia labore consequuntur dolores consectetur cum repudiandae quasi exercitationem eligendi iste? Itaque, possimus fugit.
-    Libero tempora laborum qui explicabo nesciunt! Odio, hic neque necessitatibus corrupti officiis corporis cum quam repellat, expedita ipsa doloremque tempore esse ducimus voluptates ratione sequi minus accusamus id soluta ea.
-    Beatae voluptate illum perferendis blanditiis tenetur minus suscipit odit, iure eveniet minima in, et necessitatibus ex dolorum quis adipisci. A aliquid iure porro repellat dolores itaque iusto eveniet minima voluptates?
-    Obcaecati optio neque tempore distinctio iste! Eos deleniti harum reiciendis. Porro natus recusandae, soluta accusantium adipisci, maiores voluptatibus, molestias dolore quidem dicta rem numquam laudantium quis enim! Facilis, hic tempore!
-    Dolores beatae optio cumque debitis quaerat dolorem sunt alias, quisquam ipsum expedita minima reprehenderit aliquid iste facere perspiciatis, voluptatum illo? Doloremque ab nisi rem id praesentium beatae incidunt dolorem iure.
-    Laboriosam minus cum excepturi debitis aliquam quam possimus alias minima voluptatibus. Tempore autem quia rem numquam dolorum rerum minus nam harum laudantium, dolore excepturi placeat, optio accusantium at labore enim.
-    Magni similique dolorum consectetur corrupti aperiam. Molestias ex quisquam doloribus esse sunt placeat, temporibus, praesentium sint voluptate necessitatibus soluta tenetur animi reiciendis nemo, quo pariatur sequi deserunt. Nobis, fugit qui.
-    Nihil at necessitatibus explicabo vitae veritatis. Quos optio saepe facilis repellat, accusantium, amet ipsum, deleniti porro debitis suscipit eos cupiditate earum molestias. Minus cumque enim dolorum dolores reiciendis incidunt eos.
-    Distinctio quae neque ipsa quidem esse harum. Laboriosam reiciendis possimus iste, nisi nulla molestiae. Veniam repellendus commodi labore quis eaque deserunt aliquid, maiores, quo cum vitae quidem expedita error provident.
-    Doloribus impedit rerum consequatur dolores alias labore nulla perspiciatis ratione quibusdam ullam, autem corporis similique id magnam adipisci, officiis perferendis rem at aspernatur, quod earum. Distinctio velit odit maiores magnam!
-    Eos rem dolore quasi id eligendi rerum praesentium non iste, asperiores impedit nobis, in vero. Nemo sint beatae culpa perferendis aliquam laboriosam hic ullam minima, animi, ipsum cum ipsa id!
-    Tempore eligendi quis rem, impedit consequuntur quod officia, libero officiis incidunt saepe, sunt magni culpa maiores inventore nemo quasi quos quas consectetur eius aliquid? Error nihil facilis veritatis? Facere, optio?
-    Possimus dolorum veritatis officiis ipsum nobis facilis aperiam amet ducimus nostrum dolor accusamus temporibus obcaecati magni alias sapiente culpa itaque laudantium reprehenderit natus at, dolore voluptas expedita quibusdam. Atque, ad.
-    In excepturi corrupti quod modi maxime tempore doloribus dolore, mollitia, atque alias nobis. Fugit perferendis incidunt officiis officia, adipisci quia similique maiores, reiciendis commodi recusandae repellendus ut illo eius ad!`}
-        </p>
+
+        <h1>{'Can I use the gun?'}</h1>
+        <p>{`
+          Bender, hurry! This fuel's expensive! Also, we're dying! She also
+          liked to shut up! It doesn't look so shiny to me. Ugh, it's filthy!
+          Why not create a National Endowment for Strip Clubs while we're at it?
+        `}</p>
+        <p>{`
+          We're rescuing ya. I've got to find a way to escape the horrible
+          ravages of youth.{' '}
+          <strong>
+            {' '}
+            Suddenly, I'm going to the bathroom like clockwork, every three
+            hours.
+          </strong>{' '}
+          <em>
+            {' '}
+            And those jerks at Social Security stopped sending me checks.
+          </em>{' '}
+          Now 'I'' have to pay ''them'!
+        ̀`}</p>
+        <h2>Really?!</h2>
+        <p>{`
+          You, a bobsleder!? That I'd like to see! I'm Santa Claus! Of all the
+          friends I've had… you're the first. I love you, buddy! You mean while
+          I'm sleeping in it?
+        ̀`}</p>
+        <ol>
+          <li>
+            {`Wow, you got that off the Internet? In my day, the Internet was only
+            used to download pornography.`}
+          </li>
+          <li>{`Doomsday device? Ah, now the ball's in Farnsworth's court!`}</li>
+          <li>
+            {`OK, this has gotta stop. I'm going to remind Fry of his humanity the
+            way only a woman can.`}
+          </li>
+        </ol>
+
+        <h3>{`Daddy Bender, we're hungry.`}</h3>
+        <p>{`
+          And from now on you're all named Bender Jr. Dr. Zoidberg, that doesn't
+          make sense. But, okay! Um, is this the boring, peaceful kind of taking
+          to the streets? Yeah, lots of people did.
+        ̀`}</p>
+        <ul>
+          <li>
+            {`Bender, being God isn't easy. If you do too much, people get
+            dependent on you, and if you do nothing, they lose hope. You have to
+            use a light touch. Like a safecracker, or a pickpocket.`}
+          </li>
+          <li>{`Isn't it true that you have been paid for your testimony?`}</li>
+          <li>
+            {`Hey, tell me something. You've got all this money. How come you
+            always dress like you're doing your laundry?`}
+          </li>
+        </ul>
+        <p>{`
+          Ask her how her day was. Ah, computer dating. It's like pimping, but
+          you rarely have to use the phrase "upside your head." Isn't it true
+          that you have been paid for your testimony? What kind of a father
+          would I be if I said no?
+        ̀`}</p>
+        <p>{`
+          Oh, you're a dollar naughtier than most. We'll need to have a look
+          inside you with this camera. Eeeee! Now say "nuclear wessels"! I never
+          loved you. Michelle, I don't regret this, but I both rue and lament
+          it.
+        ̀`}</p>
+        <p>{`
+          You, a bobsleder!? That I'd like to see! Oh, you're a dollar naughtier
+          than most. File not found. Daddy Bender, we're hungry. The key to
+          victory is discipline, and that means a well made bed. You will
+          practice until you can make your bed in your sleep.
+        ̀`}</p>
+        <p>{`
+          These old Doomsday Devices are dangerously unstable. I'll rest easier
+          not knowing where they are. For one beautiful night I knew what it was
+          like to be a grandmother. Subjugated, yet honored. Leela's gonna kill
+          me.
+        ̀`}</p>
+        <p>{`
+          Isn't it true that you have been paid for your testimony? I had more,
+          but you go ahead. Enough about your promiscuous mother, Hermes! We
+          have bigger problems. Ven ve voke up, ve had zese wodies. Wow! A
+          superpowers drug you can just rub onto your skin? You'd think it would
+          be something you'd have to freebase.
+        ̀`}</p>
+        <p>{`
+          Yes! In your face, Gandhi! For one beautiful night I knew what it was
+          like to be a grandmother. Subjugated, yet honored. Are you crazy? I
+          can't swallow that. Kids have names?
+        ̀`}</p>
+        <p>{`
+          Can I use the gun? I meant 'physically'. Look, perhaps you could let
+          me work for a little food? I could clean the floors or paint a fence,
+          or service you sexually? I'm just glad my fat, ugly mama isn't alive
+          to see this day.
+        ̀`}</p>
+        <p>{`
+          Have you ever tried just turning off the TV, sitting down with your
+          children, and hitting them? No, of course not. It was… uh… porno.
+          Yeah, that's it. Spare me your space age technobabble, Attila the Hun!
+        ̀`}</p>
+        <p>{`
+          One hundred dollars. But I know you in the future. I cleaned your
+          poop. The alien mothership is in orbit here. If we can hit that
+          bullseye, the rest of the dominoes will fall like a house of cards.
+          Checkmate.
+        ̀`}</p>
+        <p>{`
+          I've been there. My folks were always on me to groom myself and wear
+          underpants. What am I, the pope? Bite my shiny metal ass. Guards!
+          Bring me the forms I need to fill out to have her taken away! Hey! I'm
+          a porno-dealing monster, what do I care what you think?
+        ̀`}</p>
+        <p>{`
+          Oh, how I wish I could believe or understand that! There's only one
+          reasonable course of action now: kill Flexo! No argument here. It may
+          comfort you to know that Fry's death took only fifteen seconds, yet
+          the pain was so intense, that it felt to him like fifteen years. And
+          it goes without saying, it caused him to empty his bowels.
+        ̀`}</p>
       </div>
     </section>
   </Wrapper>
