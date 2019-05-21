5ab734a41c4dec47658dbc97df8b651052cfd904
Christopher Servius <spidergon@gmail.com>
2019-05-21 13:48:20 -0300

    corrected inner layout

--- src/components/Store/Header.js
+++ src/components/Store/Header.js
@@ -12,36 +12,34 @@ import { showSnack } from '../../lib/state'
 const Wrapper = styled.section`
   position: relative;
   border-bottom: ${props => props.theme.headerBottom};
-  .header-main-infos {
-    position: absolute;
-    grid-template-columns: auto auto;
-    bottom: 0;
-    z-index: 999;
-    background-color: #fff;
-    /* border: none ${props => props.theme.headerBottom}; */
-    /* border-bottom: none !important; */
-    margin: 0 0 10px 10px;
-    .infos {
-      display: grid;
-      width: 400px;
-      padding:  0 10px;
-      .actions {
-        align-self: end;
-        padding-bottom: 5px;
-        .loved {
-          color: ${props => props.theme.loved};
+  .infos-container {
+    .main-infos {
+      position: absolute;
+      grid-template-columns: auto auto;
+      background-color: #fff;
+      top: 30px;
+      z-index: 999;
+      .infos {
+        width: 400px;
+        padding: 0 10px;
+        .actions {
+          align-self: end;
+          padding-bottom: 5px;
+          .loved {
+            color: ${props => props.theme.loved};
+          }
+        }
+        h1 {
+          font-size: 1.5em;
+          color: #333;
+        }
+        h2 {
+          font-size: 14px;
+        }
+        .address {
+          margin-top: 5px;
         }
       }
-    }
-    h1 {
-      font-size: 1.5em;
-      color: #333;
-    }
-    h2 {
-      font-size: 14px;
-    }
-    .address {
-      margin-top: 5px;
     }
   }
 `
@@ -71,6 +69,38 @@ function Header ({ store, user }) {
 
   return (
     <Wrapper>
+      <div className='inner wrap infos-container'>
+        <article className='grid main-infos'>
+          <div className='grid infos'>
+            <div className='general'>
+              <h1 className='name no-margin'>{store.name}</h1>
+              <h2 className='activity'>{store.activity}</h2>
+              <div className='address'>{store.address}</div>
+            </div>
+            <div className='actions'>
+              <IconButton
+                aria-label={
+                  inFav ? 'Vitrine dans vos favoris' : 'Ajouter aux favoris'
+                }
+                onClick={addRemoveToFav}
+                title={
+                  inFav ? 'Vitrine dans vos favoris' : 'Ajouter aux favoris'
+                }
+              >
+                <FavoriteIcon className={inFav ? 'loved' : ''} />
+              </IconButton>
+              <IconButton
+                aria-label='Partager la vitrine sur les réseaux sociaux'
+                onClick={share}
+                title='Partager la vitrine sur les réseaux sociaux'
+              >
+                <ShareIcon />
+              </IconButton>
+            </div>
+          </div>
+          <Photos photos={store.photos} />
+        </article>
+      </div>
       <Map
         coordinates={store.coordinates}
         noLocate
@@ -78,34 +108,6 @@ function Header ({ store, user }) {
         styleSize='250px'
         zoom={17}
       />
-      <div className='header-main-infos grid'>
-        <div className='infos'>
-          <div className='general'>
-            <h1 className='name no-margin'>{store.name}</h1>
-            <h2 className='activity'>{store.activity}</h2>
-            <div className='address'>{store.address}</div>
-          </div>
-          <div className='actions'>
-            <IconButton
-              aria-label={
-                inFav ? 'Vitrine dans vos favoris' : 'Ajouter aux favoris'
-              }
-              onClick={addRemoveToFav}
-              title={inFav ? 'Vitrine dans vos favoris' : 'Ajouter aux favoris'}
-            >
-              <FavoriteIcon className={inFav ? 'loved' : ''} />
-            </IconButton>
-            <IconButton
-              aria-label='Partager la vitrine sur les réseaux sociaux'
-              onClick={share}
-              title='Partager la vitrine sur les réseaux sociaux'
-            >
-              <ShareIcon />
-            </IconButton>
-          </div>
-        </div>
-        <Photos photos={store.photos} />
-      </div>
     </Wrapper>
   )
 }
